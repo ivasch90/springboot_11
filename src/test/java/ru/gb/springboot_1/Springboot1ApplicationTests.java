5aed7031e2ee447794cea7b9396ef693b0c96477
ivasch <melmett@yandex.ru>
2022-02-10 11:05:20 +0300

    Spring Boot base

---
+++ src/test/java/ru/gb/springboot_1/Springboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.springboot_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
